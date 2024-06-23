0fb95630c9ea683434f0dec8d332e02bec3486ab
Thanh <viettran1170@gmail.com>
2024-06-23 12:58:35 +0700

    Requestdispathcher to vnpay_pay.jsp

--- ShopYouAndMe/src/java/Controller/Home/Checkout.java
+++ ShopYouAndMe/src/java/Controller/Home/Checkout.java
@@ -92,72 +92,11 @@ public class Checkout extends HttpServlet {
                         request.getRequestDispatcher("qrcode.jsp").forward(request, response);
                     }
                     if (payment_method.equals("vnpay")) {
-                        model.Bill bill = dao.getBill();
-                        String vnp_Version = "2.1.0";
-                        String vnp_Command = "pay";
-                        String orderType = "billpayment";
-                        long amount = Math.round(bill.getTotal()) * 100;
-                        String bankCode = request.getParameter("bankCode");
-
-                        String vnp_TxnRef = VnPayCommon.Config.getRandomNumber(8);
-                        String vnp_IpAddr = VnPayCommon.Config.getIpAddress(request);
-
-                        String vnp_TmnCode = VnPayCommon.Config.vnp_TmnCode;
-
-                        Map<String, String> vnp_Params = new HashMap<>();
-                        vnp_Params.put("vnp_Version", vnp_Version);
-                        vnp_Params.put("vnp_Command", vnp_Command);
-                        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
-                        vnp_Params.put("vnp_Amount", String.valueOf(amount));
-                        vnp_Params.put("vnp_CurrCode", "VND");
-
-                        if (bankCode != null && !bankCode.isEmpty()) {
-                            vnp_Params.put("vnp_BankCode", bankCode);
-                        }
-                        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
-                        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
-                        vnp_Params.put("vnp_OrderType", orderType);
-
-                        String locate = request.getParameter("language");
-                        if (locate != null && !locate.isEmpty()) {
-                            vnp_Params.put("vnp_Locale", locate);
-                        } else {
-                            vnp_Params.put("vnp_Locale", "vn");
-                        }
-                        vnp_Params.put("vnp_ReturnUrl", VnPayCommon.Config.vnp_ReturnUrl);
-                        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
-
-                        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
-                        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
-                        String vnp_CreateDate = formatter.format(cld.getTime());
-                        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
-
-                        cld.add(Calendar.MINUTE, 15);
-                        String vnp_ExpireDate = formatter.format(cld.getTime());
-                        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
-
-                        List fieldNames = new ArrayList(vnp_Params.keySet());
-                        Collections.sort(fieldNames);
-                        StringBuilder hashData = new StringBuilder();
-                        StringBuilder query = new StringBuilder();
-                        Iterator itr = fieldNames.iterator();
-                        while (itr.hasNext()) {
-                            String fieldName = (String) itr.next();
-                            String fieldValue = (String) vnp_Params.get(fieldName);
-                            if ((fieldValue != null) && (fieldValue.length() > 0)) {
-                                //Build hash data
-                                hashData.append(fieldName);
-                                hashData.append('=');
-                                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-                                //Build query
-                                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
-                                query.append('=');
-                                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
-                                if (itr.hasNext()) {
-                                    query.append('&');
-                                    hashData.append('&');
-                                }
-                            }
+                            odel.Bill bill = dao.getBill();
+                        int total = Math.round(bill.getTotal());
+                        request.setAttribute("total", total);
+                        request.setAttribute("bill", bill);
+                        request.getRequestDispatcher("vnpay_pay.jsp").forward(request, response);
                         }
                         String queryUrl = query.toString();
                         String vnp_SecureHash = VnPayCommon.Config.hmacSHA512(VnPayCommon.Config.secretKey, hashData.toString());
