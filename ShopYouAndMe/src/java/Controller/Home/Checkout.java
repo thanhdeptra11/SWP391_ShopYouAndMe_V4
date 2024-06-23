67e04d85f395560602db885082a2df4f2b06ff60
Thanh <viettran1170@gmail.com>
2024-06-23 12:57:36 +0700

    Update checkout with vnpay

--- ShopYouAndMe/src/java/Controller/Home/Checkout.java
+++ ShopYouAndMe/src/java/Controller/Home/Checkout.java
@@ -47,73 +47,150 @@ public class Checkout extends HttpServlet {
     response.setCharacterEncoding("UTF-8");
     response.setContentType("text/html; charset=UTF-8");
 
-    try {
-        HttpSession session = request.getSession(true);
-        Cart cart = (Cart) session.getAttribute("cart");
-        if (cart == null) {
-            cart = new Cart();
-        }
+     try {
 
-        User acc = (User) session.getAttribute("user");
-        if (acc == null) {
-            response.sendRedirect("user?action=login");
-            return;
-        }
+            HttpSession session = request.getSession(true);
+            Cart cart = null;
+            String payment = null;
+            billDAO dao = new billDAO();
+            String payment_method = request.getParameter("payment_method");
+            //check card
+            Object o = session.getAttribute("cart");
+            if (o != null) {
+                cart = (Cart) o;
+            } else {
+                cart = new Cart();
+            }
+            User acc = null;
+            Object u = session.getAttribute("user");
+            if (o != null) {
+                if (u != null) {
+                    String address = request.getParameter("address");
+                    String phone = request.getParameter("phone");
+                    if (payment_method.equals("momo")) {
+                        payment = "MOMO";
+                    }
+                    if (payment_method.equals("vnpay")) {
+                        payment = "VNPAY";
+                    }
+                    if (payment_method.equals("cod")) {
+                        payment = "COD";
+                    }
+                    int phonenumber = Integer.parseInt(phone);
+                    acc = (User) u;
+                    dao.addOrder(acc, cart, payment, address, phonenumber);
+                    session.removeAttribute("cart");
+                    session.setAttribute("size", 0);
+                    if (payment_method.equals("cod")) {
+                        response.sendRedirect("home");
+                    }
+                    if (payment_method.equals("momo")) {
+                        model.Bill bill = dao.getBill();
+                        int total = Math.round(bill.getTotal());
+                        request.setAttribute("total", total);
+                        request.setAttribute("bill", bill);
+                        request.getRequestDispatcher("qrcode.jsp").forward(request, response);
+                    }
+                    if (payment_method.equals("vnpay")) {
+                        model.Bill bill = dao.getBill();
+                        String vnp_Version = "2.1.0";
+                        String vnp_Command = "pay";
+                        String orderType = "billpayment";
+                        long amount = Math.round(bill.getTotal()) * 100;
+                        String bankCode = request.getParameter("bankCode");
 
-        String paymentMethod = request.getParameter("payment_method");
-        String address = request.getParameter("address");
-        String phone = request.getParameter("phone");
+                        String vnp_TxnRef = VnPayCommon.Config.getRandomNumber(8);
+                        String vnp_IpAddr = VnPayCommon.Config.getIpAddress(request);
 
-        if (address == null || phone == null || paymentMethod == null) {
+                        String vnp_TmnCode = VnPayCommon.Config.vnp_TmnCode;
+
+                        Map<String, String> vnp_Params = new HashMap<>();
+                        vnp_Params.put("vnp_Version", vnp_Version);
+                        vnp_Params.put("vnp_Command", vnp_Command);
+                        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+                        vnp_Params.put("vnp_Amount", String.valueOf(amount));
+                        vnp_Params.put("vnp_CurrCode", "VND");
+
+                        if (bankCode != null && !bankCode.isEmpty()) {
+                            vnp_Params.put("vnp_BankCode", bankCode);
+                        }
+                        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+                        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+                        vnp_Params.put("vnp_OrderType", orderType);
+
+                        String locate = request.getParameter("language");
+                        if (locate != null && !locate.isEmpty()) {
+                            vnp_Params.put("vnp_Locale", locate);
+                        } else {
+                            vnp_Params.put("vnp_Locale", "vn");
+                        }
+                        vnp_Params.put("vnp_ReturnUrl", VnPayCommon.Config.vnp_ReturnUrl);
+                        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+                        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+                        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+                        String vnp_CreateDate = formatter.format(cld.getTime());
+                        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+                        cld.add(Calendar.MINUTE, 15);
+                        String vnp_ExpireDate = formatter.format(cld.getTime());
+                        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+                        List fieldNames = new ArrayList(vnp_Params.keySet());
+                        Collections.sort(fieldNames);
+                        StringBuilder hashData = new StringBuilder();
+                        StringBuilder query = new StringBuilder();
+                        Iterator itr = fieldNames.iterator();
+                        while (itr.hasNext()) {
+                            String fieldName = (String) itr.next();
+                            String fieldValue = (String) vnp_Params.get(fieldName);
+                            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                                //Build hash data
+                                hashData.append(fieldName);
+                                hashData.append('=');
+                                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                                //Build query
+                                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                                query.append('=');
+                                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                                if (itr.hasNext()) {
+                                    query.append('&');
+                                    hashData.append('&');
+                                }
+                            }
+                        }
+                        String queryUrl = query.toString();
+                        String vnp_SecureHash = VnPayCommon.Config.hmacSHA512(VnPayCommon.Config.secretKey, hashData.toString());
+                        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+                        String paymentUrl = VnPayCommon.Config.vnp_PayUrl + "?" + queryUrl;
+                        com.google.gson.JsonObject job = new JsonObject();
+                        job.addProperty("code", "00");
+                        job.addProperty("message", "success");
+                        job.addProperty("data", paymentUrl);
+                        Gson gson = new Gson();
+                        response.getWriter().write(gson.toJson(job));
+                    }
+                
+            } 
+                else {
+                response.sendRedirect("user?action=login");
+            }
+        }else {
+                if (payment_method.equals("momo")) {
+                    model.Bill bill = dao.getBill();
+                    int total = Math.round(bill.getTotal());
+                    request.setAttribute("total", total);
+                    request.setAttribute("bill", bill);
+                    request.getRequestDispatcher("qrcode.jsp").forward(request, response);
+                }
+                if (payment_method.equals("cod")) {
+                    response.sendRedirect("home");
+                }
+            }
+    }
+    catch (Exception e) {
             request.getRequestDispatcher("404.jsp").forward(request, response);
-            return;
-        }
-
-        String payment = null;
-        switch (paymentMethod) {
-            case "momo":
-                payment = "MOMO";
-                break;
-            case "vnpay":
-                payment = "VNPAY";
-                break;
-            case "cod":
-                payment = "COD";
-                break;
-            default:
-                request.getRequestDispatcher("404.jsp").forward(request, response);
-                return;
-        }
-
-        int phoneNumber;
-        try {
-            phoneNumber = Integer.parseInt(phone);
-        } catch (NumberFormatException e) {
-            request.getRequestDispatcher("404.jsp").forward(request, response);
-            return;
-        }
-
-        billDAO dao = new billDAO();
-        dao.addOrder(acc, cart, payment, address, phoneNumber);
-
-        session.removeAttribute("cart");
-        session.setAttribute("size", 0);
-
-        switch (paymentMethod) {
-            case "cod":
-                response.sendRedirect("home");
-                break;
-            case "momo":
-                model.Bill bill = dao.getBill();
-                int total = Math.round(bill.getTotal());
-                request.setAttribute("total", total);
-                request.setAttribute("bill", bill);
-                request.getRequestDispatcher("qrcode.jsp").forward(request, response);
-                break;
-            case "vnpay":
-                request.getRequestDispatcher("VN_Pay/vn_pay.jsp").forward(request, response);
-                break;
-        }
+    }
 
     } catch (Exception e) {
         // Log the error for debugging
