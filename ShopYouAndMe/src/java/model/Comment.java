749e3b8bfbed382b2c3d85268ef2ed90f53dc12a
Doanh Tran <doanhtnhe172637@fpt.edu>
2024-06-24 16:32:26 +0700

    model.Comment product

--- ShopYouAndMe/src/java/model/Comment.java
+++ ShopYouAndMe/src/java/model/Comment.java
@@ -1,13 +1,68 @@
+package model;
+import java.sql.Timestamp;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
-
-/**
- *
- * @author admin
- */
 public class Comment {
-    
+    public int id;
+    public String productId;
+    public int userId;
+    public String comment;
+    public Timestamp createdAt;
+
+    public Comment() {
+    }
+
+    public Comment(int id, String productId, int userId,String comment, Timestamp createdAt) {
+        this.id = id;
+        this.productId = productId;
+        this.userId = userId;
+        this.comment = comment;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+   
 }
