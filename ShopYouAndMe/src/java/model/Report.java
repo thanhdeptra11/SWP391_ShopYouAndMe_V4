351ffc424570c2463ba98746e3aa2193fbb45765
Phung Gia Khanh An <anpgkhe172101@fpt.edu.vn>
2024-06-25 11:09:16 +0700

    Update Report entity

--- ShopYouAndMe/src/java/model/Report.java
+++ ShopYouAndMe/src/java/model/Report.java
@@ -1,142 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
+public class Report {
 
+    private int id_report;
+    private int user_id;
+    private String content_report;
+    private String subject_report;
+    private String user_email;
 
-public class Product {
-
-    Category cate;
-    String product_id;
-    String product_name;
-    Float product_price;
-    String product_describe;
-    int quantity;
-    String img;
-    List<Size> size = new ArrayList<>();
-    List<Color> color = new ArrayList<>();
-    Product_Active active;
-
-    public Product() {
+    public Report() {
     }
 
-    public Product(Category cate, String product_id, String product_name, Float product_price, String product_describe, int quantity, String img) {
-        this.cate = cate;
-        this.product_id = product_id;
-        this.product_name = product_name;
-        this.product_price = product_price;
-        this.product_describe = product_describe;
-        this.quantity = quantity;
-        this.img = img;
-    }
-    
-    public Product(String product_id, String product_name, Float product_price, String product_describe, int quantity, String img) {
-        this.product_id = product_id;
-        this.product_name = product_name;
-        this.product_price = product_price;
-        this.product_describe = product_describe;
-        this.quantity = quantity;
-        this.img = img;
-    }
-    
-    public Product(String product_id, String product_name, String img) {
-        this.product_id = product_id;
-        this.product_name = product_name;
-        this.img = img;
+    public Report(int id_report, int user_id, String content_report, String subject_report, String user_email) {
+        this.id_report = id_report;
+        this.user_id = user_id;
+        this.content_report = content_report;
+        this.subject_report = subject_report;
+        this.user_email = user_email;
     }
 
-    public Category getCate() {
-        return cate;
+    public int getId_report() {
+        return id_report;
     }
 
-    public void setCate(Category cate) {
-        this.cate = cate;
+    public void setId_report(int id_report) {
+        this.id_report = id_report;
     }
 
-    public String getProduct_id() {
-        return product_id;
+    public int getUser_id() {
+        return user_id;
     }
 
-    public void setProduct_id(String product_id) {
-        this.product_id = product_id;
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
     }
 
-    public String getProduct_name() {
-        return product_name;
+    public String getContent_report() {
+        return content_report;
     }
 
-    public void setProduct_name(String product_name) {
-        this.product_name = product_name;
+    public void setContent_report(String content_report) {
+        this.content_report = content_report;
     }
 
-    public Float getProduct_price() {
-        return product_price;
+    public String getSubject_report() {
+        return subject_report;
     }
 
-    public void setProduct_price(Float product_price) {
-        this.product_price = product_price;
+    public void setSubject_report(String subject_report) {
+        this.subject_report = subject_report;
     }
 
-    public String getProduct_describe() {
-        return product_describe;
+    public String getUser_email() {
+        return user_email;
     }
 
-    public void setProduct_describe(String product_describe) {
-        this.product_describe = product_describe;
+    public void setUser_email(String user_email) {
+        this.user_email = user_email;
     }
 
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getImg() {
-        return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
-    }
-
-    public List<Size> getSize() {
-        return size;
-    }
-
-    public void setSize(List<Size> size) {
-        this.size = size;
-    }
-
-    public List<Color> getColor() {
-        return color;
-    }
-
-    public void setColor(List<Color> color) {
-        this.color = color;
-    }
-
-    public Product_Active getActive() {
-        return active;
-    }
-
-    public void setActive(Product_Active active) {
-        this.active = active;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "Product{" + "cate=" + cate + ", product_id=" + product_id + ", product_name=" + product_name + ", product_price=" + product_price + ", product_describe=" + product_describe + ", quantity=" + quantity + ", img=" + img + ", size=" + size + ", color=" + color + '}';
-    }
-
-   
-        
 }
