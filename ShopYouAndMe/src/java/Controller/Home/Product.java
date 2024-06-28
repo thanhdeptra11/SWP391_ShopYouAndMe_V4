24edc86d9ce61bf1b2816357a84b2716363782f2
namphhe176612@fpt.edu.vn <namphhe176612@fpt.edu.vn>
2024-06-28 07:51:16 +0700

    change controller home product

--- ShopYouAndMe/src/java/Controller/Home/Product.java
+++ ShopYouAndMe/src/java/Controller/Home/Product.java
@@ -7,8 +7,6 @@ package Controller.Home;
 
 import dal.productDAO;
 import model.Category;
-import model.Color;
-import model.Size;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
