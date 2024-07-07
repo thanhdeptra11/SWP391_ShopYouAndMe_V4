ae596f725eff7278f7b89d3410ea54788afe951c
Thanh <viettran1170@gmail.com>
2024-07-07 10:00:40 +0700

    Add BillRubish model

---
+++ ShopYouAndMe/src/java/model/BillRubish.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Thanh
+ */
+public class BillRubish {
+    User user;
+    Cart cart;
+    String payment;
+    String address;
+    String phone;
+
+    public BillRubish() {
+    }
+
+    public BillRubish(User user, Cart cart, String payment, String address, String phone) {
+        this.user = user;
+        this.cart = cart;
+        this.payment = payment;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    
+}
